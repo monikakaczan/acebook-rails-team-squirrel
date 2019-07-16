f83a3d15c722013377c3f9df37ac64449c80b4ca
leon.linton@live.co.uk <leon.linton@live.co.uk>
2019-07-16 13:16:23 +0100

    Current User Helper Method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  root 'home#index'
 
   resources :users
-  root 'home#index'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
