59d31083775183de2ebb3bdf29b52ac60c9b3651
Jake Pilikian <47749795+binaryred01@users.noreply.github.com>
2019-07-26 14:34:28 +0100

    Update users_login_spec.rb

--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -21,30 +21,6 @@ RSpec.describe "Log in", type: :feature do
     expect(page).to have_content("Email or password is invalid")
   end
 
-  scenario "will let an existing user edit their details" do
-    sign_up
-    expect(page).to have_content("User was successfully created.")
-
-    click_link "Edit"
-    fill_in "user_email", with: "email@email.com"
-    fill_in "user_password", with: "qwerty"
-    fill_in "user_password_confirmation", with: "qwerty"
-    click_button "Update User"
-    expect(page).to have_content("User was successfully updated.")
-  end
-
-  scenario "will raise an error if edited user details do not match" do
-    sign_up
-    expect(page).to have_content("User was successfully created.")
-
-    click_link "Edit"
-    fill_in "user_email", with: "email@email.com"
-    fill_in "user_password", with: "qwerty"
-    fill_in "user_password_confirmation", with: "qwertyuiop"
-    click_button "Update User"
-    expect(page).to have_content("Password confirmation doesn't match Password")
-  end
-
   # scenario "will let a logged in user log out" do
   #   sign_up
   #   expect(page).to have_content("User was successfully created.")
